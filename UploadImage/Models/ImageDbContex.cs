a5e06f05a996ef5ec0ae38efac3af1931b96bd14
Vagner Bom Jesus <vagneripg@gmail.com>
2020-11-30 13:53:46 +0000

    Add project files.

---
+++ UploadImage/Models/ImageDbContex.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadImage.Models
+{
+    public class ImageDbContex: DbContext
+    {
+        public ImageDbContex (DbContextOptions<ImageDbContex> options): base(options) { }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
---
+++ UploadImage/Models/ImageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadImage.Models
+{
+    public class ImageModel
+    {
+        [Key]
+       
+        public int ImageId { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageIname { get; set; }
+    }
+}
